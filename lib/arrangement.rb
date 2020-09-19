0a3add836d161a26bccc5d2b2f9478d28a1be615
Tad Thorley <phaedryx@gmail.com>
2020-09-18 23:56:35 -0600

    initial commit

---
+++ lib/arrangement.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'arrangement/version'
+
+module Arrangement
+  class Error < StandardError; end
+end
---
+++ lib/arrangement/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Arrangement
+  VERSION = '0.0.1'
+end
