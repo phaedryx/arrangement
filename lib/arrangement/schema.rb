464e86de429090bf3efed9ad858c6bebafd7152f
Tad Thorley <phaedryx@gmail.com>
2020-10-10 17:20:39 -0600

    adds yard-style documentation to Schema

--- lib/arrangement/schema.rb
+++ lib/arrangement/schema.rb
@@ -2,13 +2,24 @@
 
 module Arrangement
   ##
-  # TODO: explain this when it does more
-  #
+  # The Arrangement::Schema class is a specialized Hash. The difference is that it returns the call
+  # value of a value if a value is callable.
   class Schema < Hash
+    ##
+    # This returns the value of a call if it is callable, otherwise the value. This is the key
+    # behavior that sets Schema appart from a regular Hash.
+    #
+    # @param value [Object]
+    # @return [Object]
     def call_value(value)
       value.respond_to?(:call) ? value.call : value
     end
 
+    ##
+    # This is the same functionality as `call_value`, but across a collection (`Array` or `Hash`).
+    #
+    # @param collection [Array|Hash]
+    # @return [Object]
     def call_collect(collection)
       case collection
       when Hash then collection.transform_values { |v| call_collect(v) }
@@ -25,25 +36,28 @@ module Arrangement
       call_value(super)
     end
 
-    def clone
-      call_collect(super)
-    end
-
-    def dup
-      call_collect(super)
-    end
-
+    # @return [Hash]
     def to_h
       call_collect(super)
     end
 
     class << self
+      ##
+      # This transforms a yaml-style string into an instace of `Arrangement::Schema`.
+      #
+      # @param string [String]
+      # @return [Arrangement::Schema]
       def load(string)
         hash = YAML.safe_load(string) || {}
 
         transform(hash)
       end
 
+      ##
+      # This loads a yaml file and transforms it into an instance of `Arrangement::Schema`.
+      #
+      # @param path [String] the path to the file to load
+      # @return [Arrangement::Schema]
       def load_file(path)
         load(File.read(path))
       end
@@ -63,7 +77,8 @@ module Arrangement
           # rubocop:enable Security/Eval
         end
       end
-      private :transform # method with an eval should be private
+      # make this method private because it eval's
+      private :transform
     end
   end
 end
